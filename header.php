5e7373f91b474b3009af224a799748479bfc0e36
Christgb <srsteve1913@gmail.com>
2021-08-28 00:21:10 -0400

    Cambios minimos

---
+++ header.php
@@ -0,0 +1,21 @@
+<header id="header" class="d-flex align-items-center ">
+  <div class="container-fluid container-xxl d-flex align-items-center">
+    <div id="logo" class="me-auto">
+        <a href="index.html" class="scrollto"><img src="/img/logo.png" alt="" title=""></a>
+    </div>
+    <nav id="navbar" class="navbar order-last order-lg-0 sticky-top">
+        <ul>
+            <li><a class="nav-link scrollto active" href="#home">Home</a></li>
+            <li><a class="nav-link scrollto" href="#real">Real Estate</a></li>
+            <li><a class="nav-link scrollto" href="#property">Property</a></li>
+            <li><a class="nav-link scrollto" href="#Elementor">Elementor</a></li>
+            <li><a class="nav-link scrollto" href="#Blog">Blog</a></li>
+            <li><a class="nav-link scrollto" href="#Other">Other</a></li>
+            <li><a class="nav-link scrollto" href="#Contact">Contact</a></li>
+            <li><a class="nav-link scrollto" href="#tel"><img src="/img/tell-icon.svg" alt=""></a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav><!-- .navbar -->
+    <a class="submit scrollto" href="#submit">Submit</a>
+  </div>
+</header>
